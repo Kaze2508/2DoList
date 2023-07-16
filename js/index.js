e41fcbff54540f38f3a15479b4eed72489a58fa3
Kaze Phake <phongsgvn@gmail.com>
2023-07-16 15:52:20 +0700

    Update Entry Node

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 let editMode = false; // Variable to track edit mode
 
+var num = 0;
 
 var firebaseConfig = {
   // Replace with your own Firebase configuration
@@ -17,53 +18,179 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
     // Get a reference to the Firebase Realtime Database
+// var db = firebase.database();
+
+// var tasksRef = db.ref().child("tasks");
+
+// function handleKeyPress(event) {
+//   if (event.keyCode === 13) {
+//       event.preventDefault(); // Prevent form submission
+//       addTask();
+//   }
+// }
+
+// function addTask() {
+//   if (inputBox.value === '') {
+//     alert("You must write something!");
+//     return; // Exit the function if input is empty
+//   }
+
+//   const task = document.createElement("div");
+//   task.className = "task";
+//   var text = document.getElementById("input-box").value;
+
+//   // var newKey = db.ref().push().key;
+
+//   // Create a new data object
+//   var newData = {
+//     text: text
+//   };
+
+//   // Save the new data to the database
+//   // db.ref('texts/' + newKey).set(newData)
+//   db.ref('tasks/').set(newData)
+//     // .then(function() {
+//     //   alert("Text saved successfully!");
+//     //   document.getElementById("textInput").value = "";
+//     // })
+//     // .catch(function(error) {
+//     //   alert("Error saving text: " + error);
+//     // });
+
+//   const li = document.createElement("li");
+//   li.textContent = escapeHTML(inputBox.value);
+
+//   const checkbox = document.createElement("input");
+//   checkbox.type = "checkbox";
+//   checkbox.addEventListener("change", toggleTaskCompletion);
+
+//   li.addEventListener("click", function() {
+//     if (!editMode) {
+//       enableEditMode(li);
+//     }
+//   });
+
+//   const editButton = document.createElement("i");
+//   editButton.classList.add("fa-solid", "fa-pen-to-square");
+//   editButton.addEventListener("click", function() {
+//     if (!editMode) {
+//       enableEditMode(li);
+//     } else {
+//       disableEditMode(li);
+//     }
+//     editMode = !editMode;
+//   });
+
+//   const deleteButton = document.createElement("i");
+//   deleteButton.classList.add("fa-regular", "fa-trash-can");
+//   deleteButton.addEventListener("click", function() {
+//     const confirmation = confirm("Are you sure?");
+//     if (confirmation) {
+//       task.remove();
+//     }
+//   });
+
+//   task.appendChild(checkbox);
+//   task.appendChild(li);
+//   task.appendChild(editButton);
+//   task.appendChild(deleteButton);
+//   listContainer.appendChild(task);
+
+//   inputBox.value = '';
+// }
+
+// function toggleTaskCompletion() {
+//   const li = this.nextElementSibling;
+//   if (!editMode) {
+//     li.style.textDecoration = this.checked ? "line-through" : "none";
+//   } else {
+//     this.checked = false;
+//   }
+// }
+
+// function enableEditMode(li) {
+//   li.contentEditable = true;
+//   li.classList.add("editable");
+//   placeCursorAtEnd(li);
+// }
+
+// function placeCursorAtEnd(element) {
+//   const range = document.createRange();
+//   const selection = window.getSelection();
+//   range.selectNodeContents(element);
+//   range.collapse(false);
+//   selection.removeAllRanges();
+//   selection.addRange(range);
+//   element.focus();
+// }
+
+// function disableEditMode(li) {
+//   li.contentEditable = false;
+//   li.classList.remove("editable");
+// }
+
+// document.addEventListener("keydown", function(event) {
+//   if (event.keyCode === 13 && editMode) {
+//     event.preventDefault();
+//     const editableTask = document.querySelector(".task .editable");
+//     if (editableTask) {
+//       disableEditMode(editableTask);
+//       editMode = false;
+//     }
+//   }
+// });
+
+// // Utility function to escape HTML characters
+// function escapeHTML(text) {
+//   const element = document.createElement('div');
+//   element.textContent = text;
+//   return element.innerHTML;
+// }
+
 var db = firebase.database();
 
 var tasksRef = db.ref().child("tasks");
 
+
+
 function handleKeyPress(event) {
   if (event.keyCode === 13) {
-      event.preventDefault(); // Prevent form submission
-      addTask();
+    event.preventDefault(); // Prevent form submission
+    addTask();
   }
 }
 
 function addTask() {
-  if (inputBox.value === '') {
+  if (inputBox.value === "") {
     alert("You must write something!");
     return; // Exit the function if input is empty
   }
 
+  // var text = inputBox.value.trim();
+  var text = inputBox.value;
+
+  num++;
+
+  // Generate a timestamp for the task
+  // var timestamp = new Date().getTime();
+
+  // Create a new child reference with the timestamp as the key
+  var newTaskRef = tasksRef.child(num);
+
+  // Set the task text as the value of the new child reference
+  newTaskRef.set(text);
+
   const task = document.createElement("div");
   task.className = "task";
-  var text = document.getElementById("input-box").value;
-
-  // var newKey = db.ref().push().key;
-
-  // Create a new data object
-  var newData = {
-    text: text
-  };
-
-  // Save the new data to the database
-  // db.ref('texts/' + newKey).set(newData)
-  db.ref('tasks/').set(newData)
-    // .then(function() {
-    //   alert("Text saved successfully!");
-    //   document.getElementById("textInput").value = "";
-    // })
-    // .catch(function(error) {
-    //   alert("Error saving text: " + error);
-    // });
 
   const li = document.createElement("li");
-  li.textContent = escapeHTML(inputBox.value);
+  li.textContent = escapeHTML(text);
 
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.addEventListener("change", toggleTaskCompletion);
 
-  li.addEventListener("click", function() {
+  li.addEventListener("click", function () {
     if (!editMode) {
       enableEditMode(li);
     }
@@ -71,7 +198,7 @@ function addTask() {
 
   const editButton = document.createElement("i");
   editButton.classList.add("fa-solid", "fa-pen-to-square");
-  editButton.addEventListener("click", function() {
+  editButton.addEventListener("click", function () {
     if (!editMode) {
       enableEditMode(li);
     } else {
@@ -82,7 +209,7 @@ function addTask() {
 
   const deleteButton = document.createElement("i");
   deleteButton.classList.add("fa-regular", "fa-trash-can");
-  deleteButton.addEventListener("click", function() {
+  deleteButton.addEventListener("click", function () {
     const confirmation = confirm("Are you sure?");
     if (confirmation) {
       task.remove();
@@ -95,7 +222,7 @@ function addTask() {
   task.appendChild(deleteButton);
   listContainer.appendChild(task);
 
-  inputBox.value = '';
+  inputBox.value = "";
 }
 
 function toggleTaskCompletion() {
@@ -128,7 +255,7 @@ function disableEditMode(li) {
   li.classList.remove("editable");
 }
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
   if (event.keyCode === 13 && editMode) {
     event.preventDefault();
     const editableTask = document.querySelector(".task .editable");
@@ -141,7 +268,7 @@ document.addEventListener("keydown", function(event) {
 
 // Utility function to escape HTML characters
 function escapeHTML(text) {
-  const element = document.createElement('div');
+  const element = document.createElement("div");
   element.textContent = text;
   return element.innerHTML;
 }
